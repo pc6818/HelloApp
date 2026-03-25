7690f6d021bacdb4b22db917234b22b942574142
pc6818 <pc6818@srmist.edu.in>
2026-03-26 04:31:51 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+
+        String name = "world";
+
+        if (args.length > 0){
+            name = args[0];
+        }
+
+        System.out.println("Hello, " + name +"!");
     }
 }
